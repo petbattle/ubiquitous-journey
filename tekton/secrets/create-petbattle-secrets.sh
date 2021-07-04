ae1dfb076748a65c5c6797c02d97056f654a5f68
Mike Hepburn <eformat@gmail.com>
2021-07-04 14:15:46 +1000

    🦙 use labs-ci-cd project 🦙

--- tekton/secrets/create-petbattle-secrets.sh
+++ tekton/secrets/create-petbattle-secrets.sh
@@ -76,6 +76,8 @@ if [ -z ${GITHUB_TOKEN} ] || [ -z ${WEBHOOK_SECRET} ] || [ -z ${ARGOCD_USERNAME}
     usage
 fi
 
+oc project labs-ci-cd
+
 oc delete pod -l app.kubernetes.io/name=argocd-server
 oc wait pod -l app.kubernetes.io/name=argocd-server --for=condition=Ready --timeout=100s
 
